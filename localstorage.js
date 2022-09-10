5a5dcf8259f5ef697dbee9fb8c9343212e8aed00
MudasirSherwani <mudasir9757@gmail.com>
2022-09-10 12:57:25 +0500

    Add JS function to store contact data

--- localstorage.js
+++ localstorage.js
@@ -1,23 +1,10 @@
-const userName = document.getElementById('name');
-const userEmail = document.getElementById('mail');
-const userMsg = document.getElementById('message');
-// storing users'Data in the local storage
-const populateLocalStorage = () => {
-  const formData = {
-    userName: userName.value,
-    userEmail: userEmail.value,
-    userMsg: userMsg.value,
-  };
-  window.localStorage.setItem('userData', JSON.stringify(formData));
-};
-// Retrieving the stored data in local storage
-const fetchData = () => {
-  const storageData = JSON.parse(window.localStorage.getItem('userData'));
-  userName.value = storageData.userName;
-  userEmail.value = storageData.userEmail;
-  userMsg.value = storageData.userMsg;
-};
-fetchData();
-userName.addEventListener('change', populateLocalStorage);
-userEmail.addEventListener('change', populateLocalStorage);
-userMsg.addEventListener('change', populateLocalStorage);
+
+function DataStoring(name, email, message){
+    const formField = {StoredName: name, StoredEmail: email, StoredMsg: message};
+    localStorage.setItem('ContactDataStorage', JSON.stringify(formField));
+    return true;
+  }
+const StoredContact = JSON.parse(localStorage.getItem('ContactDataStorage'));
+document.getElementById('full-name').value = StoredContact.StoredName;
+document.getElementById('mail').value = StoredContact.StoredEmail;
+document.getElementById('message').value = StoredContact.StoredMsg;
