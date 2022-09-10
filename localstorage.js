5a24d0f5f4cad15ce88a20f549896d20991dd9df
Kaweesi Matia <matiakaweesi@gmail.com>
2022-09-10 02:15:25 -0700

    fix linter errors

--- localstorage.js
+++ localstorage.js
@@ -1,13 +1,11 @@
-
-function DataStoring(name, email, message){
-    const formField = {StoredName: name, StoredEmail: email, StoredMsg: message};
-    localStorage.setItem('ContactDataStorage', JSON.stringify(formField));
-    return true;
-  }
+function DataStoring(name, email, message) {
+  const formField = { StoredName: name, StoredEmail: email, StoredMsg: message };
+  localStorage.setItem('ContactDataStorage', JSON.stringify(formField));
+  return true;
+}
 const StoredContact = JSON.parse(localStorage.getItem('ContactDataStorage'));
 document.getElementById('full-name').value = StoredContact.StoredName;
 document.getElementById('mail').value = StoredContact.StoredEmail;
 document.getElementById('message').value = StoredContact.StoredMsg;
 const x = '';
 if (x !== '') { DataStoring(x, x, x); }
-
